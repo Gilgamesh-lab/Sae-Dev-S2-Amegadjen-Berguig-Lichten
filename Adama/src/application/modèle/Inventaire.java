58e7e60060cf8c493fc85b145e7fca1deec79fe4
ShimmerPlume <94871035+ShimmerPlume@users.noreply.github.com>
2022-05-16 09:31:45 +0200

    Ajout du model
    Le modele est mis au bonne endroit

---
+++ Adama/src/application/modèle/Inventaire.java
@@ -0,0 +1,61 @@
+package application.modele;
+
+import java.util.ArrayList;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Inventaire {
+	private ObservableList<Item> items;
+	private IntegerProperty tailleMaxProperty;
+
+	
+	public Inventaire(int taille) {
+		this.items = FXCollections.observableArrayList();
+		this.tailleMaxProperty = new SimpleIntegerProperty(taille);
+	}
+	
+	public ObservableList<Item> getItems() {
+		return this.items;
+	}
+	
+	public final int getTailleMax() {
+		return this.tailleMaxProperty.getValue();
+	}
+	
+	public final void setTailleMax(int val) {
+		this.tailleMaxProperty.setValue(val);
+	}
+	
+	public final IntegerProperty tailleMaxProperty() {
+		return this.tailleMaxProperty;
+	}
+	
+	public int getTaille() {
+		return this.items.size();
+	}
+	
+	public void ajouter(Item item) {// erreur à coder
+		if(!estPleine()) {
+			this.items.add(item);
+		}
+	}
+	
+	public void supprimer(Item item) {
+		this.items.remove(item);
+	}
+	
+	public void remplacer(Item item, int indice) {
+		this.items.add(indice, item);
+	}
+	
+	public void supprimerAvecIndice(int indice) {
+		this.items.remove(indice);
+	}
+	
+	public boolean estPleine() {
+		return this.items.size() == this.getTaille();
+	}
+}
---
+++ Adama/src/application/modèle/Joueur.java
@@ -0,0 +1,65 @@
+package application.modele;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class Joueur extends Personnage {
+	
+	private final static int MAX_PV = 7;
+	private IntegerProperty faimProperty;
+	private final static int MAX_FAIM = 7;
+	private Item objetEquiper;
+	private Inventaire inventaireRaccourci;
+
+
+
+	public Joueur(int pv,int x, int y,
+	Environnement carte, int faim, Inventaire inventaire,
+	Item objetEquiper, Inventaire inventaireRaccourci) {
+		super(pv, x, y,5, carte,inventaire);
+		this.faimProperty = new SimpleIntegerProperty(faim);
+		this.objetEquiper = objetEquiper;
+		this.inventaireRaccourci = inventaireRaccourci;
+	}
+
+	public Joueur(int pv,int x, int y,
+			Environnement carte) {
+		super(MAX_PV, x, y,5, carte);
+		this.faimProperty = new SimpleIntegerProperty(MAX_FAIM);
+		this.objetEquiper = null;
+		this.inventaireRaccourci = new Inventaire(10);
+	}
+
+	public final int getFaim() {
+		return this.faimProperty.getValue();
+	}
+
+	public final void setFaim(int val) {
+		this.faimProperty.setValue(val);
+	}
+
+	public final IntegerProperty faimProperty() {
+		return this.faimProperty;
+	}
+
+
+	public void setObjetEquiper(Item item) {
+		this.objetEquiper = item;
+	}
+
+	public void remplacerObjetRaccourci(int indice, Item item) {
+		this.inventaireRaccourci.remplacer(item, indice);
+	}
+
+	public void equiper(Item item) {
+		this.objetEquiper = item;
+	}
+
+	public void desequiper() {
+		// Arme main =  new Poing();
+		// this.objetEquiper = main;
+	}
+
+
+
+}
