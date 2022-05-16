c2e93454fb39e0f5d20cb49ec2fdb8796e17e65d
ShimmerPlume <94871035+ShimmerPlume@users.noreply.github.com>
2022-05-16 09:32:52 +0200

    Ajout des elements de la vue
    Les elements de la vue sont mis au bonne endroit

---
+++ Adama/src/application/vue/EnvironnementVue.java
@@ -0,0 +1,37 @@
+package application.vue;
+
+import application.modele.Environnement;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
+
+public class EnvironnementVue {
+	
+	private final static int PIXEL=32;//Ecran de l'iut 4*480 largeur 1920*1080 16/9
+	private TilePane carte;
+	private Environnement env;
+	
+	public EnvironnementVue(Environnement env, TilePane carte) {
+		this.carte=carte;
+		this.env=env;
+	}
+	
+	public void creerEnvironnement() {
+		carte.setPrefHeight(PIXEL);
+		for(int[] tab : Environnement.grille())
+			for (int val : tab)
+				carte.getChildren().add(choixTuile(val));
+	}
+	
+	private ImageView choixTuile(int val) {
+		ImageView img;
+		if (val==0)
+			img = new ImageView("ressource/bleu.jpeg");
+		else if (val==1)
+			img = new ImageView("ressource/marron.jpeg");
+		else
+			img = new ImageView("ressource/herbe.png");
+		img.setFitHeight(PIXEL);
+		img.setFitWidth(PIXEL);
+		return img;
+	}
+}
