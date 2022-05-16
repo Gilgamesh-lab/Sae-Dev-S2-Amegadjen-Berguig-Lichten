03b283e30b45014661f7daf0fd676d0aba001a1b
llichten29 <llichten29@gmail.com>
2022-05-16 14:37:30 +0200

    j'ai débbuger le prog

--- Adama/src/application/controleur/Controleur.java
+++ Adama/src/application/controleur/Controleur.java
@@ -9,16 +9,24 @@ import javafx.beans.property.IntegerProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.TilePane;
 
 public class Controleur implements Initializable{
 
 	@FXML
 	private TilePane carte;
+	
+	@FXML
+	private ImageView perso;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		EnvironnementVue env = new EnvironnementVue(new Environnement(), carte);
 		env.creerEnvironnement();
+	}
+	@FXML
+	void onAction(KeyEvent event) {
+		
 	}
 }
