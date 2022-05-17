274119c942419a1b41be3e1c2bdd2ddecaa93a1c
Fabrice <83886034+Gilgamesh-lab@users.noreply.github.com>
2022-05-17 17:00:45 +0200

    Ajout de nouvelles méthodes

--- Adama/src/application/modele/Inventaire.java
+++ Adama/src/application/modele/Inventaire.java
@@ -1,11 +1,12 @@
 package application.modele;
 
-import java.util.ArrayList;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
+
 
 public class Inventaire {
 	private ObservableList<Item> items;
@@ -19,6 +20,10 @@ public class Inventaire {
 	
 	public ObservableList<Item> getItems() {
 		return this.items;
+	}
+	
+	public Item getItem(int indice) {
+		return this.items.get(indice);
 	}
 	
 	public final int getTailleMax() {
@@ -37,9 +42,12 @@ public class Inventaire {
 		return this.items.size();
 	}
 	
-	public void ajouter(Item item) {// erreur à coder
-		if(!estPleine()) {
+	public void ajouter(Item item) throws ErreurInventairePlein {
+		if(!estPlein()) {
 			this.items.add(item);
+		}
+		else {
+			throw new ErreurInventairePlein();
 		}
 	}
 	
@@ -47,15 +55,38 @@ public class Inventaire {
 		this.items.remove(item);
 	}
 	
-	public void remplacer(Item item, int indice) {
-		this.items.add(indice, item);
-	}
-	
-	public void supprimerAvecIndice(int indice) {
+	public void supprimer(int indice) {
 		this.items.remove(indice);
 	}
 	
-	public boolean estPleine() {
-		return this.items.size() == this.getTaille();
+	public void remplacer(Item item, int indice) { // objet ramassé qui remplace un objet présent dans l'inventaire qui sera jeté
+		this.items.add(indice, item);
 	}
+	
+	public void echanger(Item item1, int indice1, Item item2, int indice2) {// échanger  deux objets déjà dans l'inventaire
+		this.remplacer(item1, indice2);
+		this.remplacer(item2, indice1);
+	}
+	
+	public boolean estPlein() {
+		return this.getTaille() >= this.getTailleMax();
+	}
+	
+	
+	public void transfererInventaire(Inventaire inventaireSource) throws ErreurInventairePlein {
+		for (int i = 0 ; i < inventaireSource.getTaille() ; i++) {
+			this.ajouter(inventaireSource.getItem(i));
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
