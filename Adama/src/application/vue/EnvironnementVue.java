74c2f51441016b9dd4c61a0be8c6fb75baeafdff
llichten29 <llichten29@gmail.com>
2022-05-19 11:54:24 +0200

    Simplification de choixTuile et suppression des fichiers CSV inutiles

--- Adama/src/application/vue/EnvironnementVue.java
+++ Adama/src/application/vue/EnvironnementVue.java
@@ -5,31 +5,49 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.TilePane;
 
 public class EnvironnementVue {
-	
-//	public final static int PIXEL=32;//Ecran de l'iut 4*480 largeur 1920*1080 16/9
+
+	//	public final static int PIXEL=32;//Ecran de l'iut 4*480 largeur 1920*1080 16/9
 	private TilePane carte;
 	private Environnement env;
-	
+
 	public EnvironnementVue(Environnement env, TilePane carte) {
 		this.carte=carte;
 		this.env=env;
 	}
-	
+
 	public void creerEnvironnement() {
-//		carte.setPrefHeight(PIXEL);
-		for(int[] tab : env.getCarte().getCarteTab())
-			for (int val : tab)
-				carte.getChildren().add(choixTuile(val));
+		int val;
+		while(env.getCarte().getMap().hasNext()) {
+			val = Integer.parseInt(env.getCarte().getMap().next());
+			carte.getChildren().add(choixTuile(val));
+		}
 	}
-	
+
 	private ImageView choixTuile(int val) {
-		ImageView img;
-		if (val==0)
+		ImageView img = null;
+		switch(val){
+		case 0:
 			img = new ImageView("ressource/bleu.jpeg");
-		else if (val==1)
-			img = new ImageView("ressource/marron.jpeg");
-		else
+			break;
+		case 1:
 			img = new ImageView("ressource/herbe.jpeg");
+			break;
+		case 2:
+			img = new ImageView("ressource/marron.jpeg");
+			break;
+		case 3:
+			img = new ImageView("ressource/Granit.jpeg");
+			break;
+		case 4:
+			img = new ImageView("ressource/nuage.jpg");
+			break;
+		case 5:
+			img = new ImageView("ressource/Soleil.jpg");
+			break;
+		default:
+			System.out.println("La valeur" + val + "ne correpond à aucune tuiles !");
+			break;
+		}
 		return img;
 	}
 }
