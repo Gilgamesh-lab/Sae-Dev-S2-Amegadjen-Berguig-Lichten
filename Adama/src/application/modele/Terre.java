8077f466e3df36eb7fe220d9635b73c2901a623e
llichten29 <llichten29@gmail.com>
2022-05-20 10:59:12 +0200

    Mise à jour du constructeur terre

--- Adama/src/application/modele/Terre.java
+++ Adama/src/application/modele/Terre.java
@@ -1,8 +1,10 @@
 package application.modele;
 
 public class Terre extends Ressource {
+	
+	private static final int durabiliter = 10; //quand on pose le bloc combien en combien de coup les ennemis le casse
 
-	public Terre(int nombre, int durabiliter, int x, int y, boolean posable) {
+	public Terre(int nombre, int x, int y, boolean posable) {
 		super(nombre, durabiliter, x, y, posable);
 	}
 
