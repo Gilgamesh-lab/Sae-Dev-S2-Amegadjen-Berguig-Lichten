1c8c2c6c2c2aba9b4b08fc2bf5e49c59a4ff2047
Fabrice <83886034+Gilgamesh-lab@users.noreply.github.com>
2022-05-24 10:59:06 +0200

    Carte
    Ajout de fonctionnalités pour détruire les blocs

--- Adama/src/application/modele/Carte.java
+++ Adama/src/application/modele/Carte.java
@@ -1,87 +1,73 @@
 package application.modele;
+import application.modele.exception.TailleMapException;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import application.modele.exception.TailleMapException;
+
+import application.modele.exception.ErreurInventairePlein;
+import application.modele.exception.ErreurObjetIntrouvable;
+
 
 public class Carte {
-
 	private BufferedReader map;
 	private final static int HAUTEUR = 32;
 	private final static int LARGEUR = 60;
-	private final static int TAILLE_BLOCK = 32;
-	private ArrayList<Ressource> blockMap;
-
-	public Carte() throws TailleMapException, IOException {
-		this.map = Csv.ouvrir("testMap.csv");
-		this.blockMap = new ArrayList<Ressource>();
-		creerListeBlock();
-	}
-
-	public int getHauteur() {
-		return HAUTEUR;
-	}
-
-	public int getLargeur() {
-		return LARGEUR;
-	}
-
-	public ArrayList<Ressource> getBlockMap() {
-		return blockMap;
-	}
-
-	public static int getTailleBlock() {
-		return TAILLE_BLOCK;
+	private ArrayList<Ressource> blockMap; // 
+	private Inventaire poubelle;
+	 
+	public Carte(String nomFichier) throws IOException, TailleMapException, ErreurInventairePlein {
+		this.map = Csv.ouvrir(nomFichier);
+		this.blockMap = new ArrayList<Ressource>(99);
+		this.poubelle = new Inventaire(99);
+		this.creerListeBlock();
 	}
 	
-	/**
-	 * 
-	 * @param x
-	 * @param y
-	 * @param taille
-	 * @return
-	 */
-	public Ressource emplacement(int x, int y, int[] taille) {
-		int indiceDansMap = (x/TAILLE_BLOCK) + ((y/TAILLE_BLOCK) * LARGEUR);
-		return this.blockMap.get(indiceDansMap);
+	public BufferedReader getMap(){
+		return this.map;
+	} 
+	
+	public int getHauteur() {
+		return Carte.HAUTEUR;
 	}
-
-	/**
-	 * 
-	 * @throws TailleMapException
-	 * @throws IOException
-	 */
-	public void creerListeBlock() throws TailleMapException, IOException{
+	
+	public int getLargeur() {
+		return Carte.LARGEUR;
+	}
+	
+	public ArrayList<Ressource> getInventaire() {
+		return this.blockMap;
+	}
+	
+	public void creerListeBlock() throws TailleMapException, IOException, ErreurInventairePlein{
 		String ligne;
 		char suivant;
 		int x = 0;
 		int y = 0;
 		ligne = this.map.readLine();
-
-		//System.out.println(ligne.length());
 		while(ligne!=null) {
-			for (int indice=0; indice<ligne.length(); indice=indice+2) {
+			for (int indice=0; indice<ligne.length()-1; indice++) {
 				suivant=ligne.charAt(indice);
 				switch (suivant) {
 					case '1':
-						blockMap.add(new Terre(x*TAILLE_BLOCK, y*TAILLE_BLOCK, false));
+						blockMap.add(new Terre(x, y));
 						break;
 					case '2':
-						blockMap.add(new Terre(x*TAILLE_BLOCK, y*TAILLE_BLOCK, false));
+						blockMap.add(new Terre(x, y));
 						break;
 					case '3':
-						blockMap.add(new Pierre(x*TAILLE_BLOCK, y*TAILLE_BLOCK, false));
+						blockMap.add(new Pierre(x, y, false));
 						break;
-//					case ',':
-//						x++;
-//						break;
+					case ',':
+						x++;
+						break;
 					default://tous las chiffres de tuile avec lesquelles on ne peut intéragir (ciel, nuage,...)
 						blockMap.add(null);
 						break;
 				}	
-				x++;
 			}
+			x++;
 			if (x!=LARGEUR)
 				throw new TailleMapException("Problème de Largeur : "+x+" a la place des "+LARGEUR+" demandés.");
 			x=0;
@@ -90,7 +76,49 @@ public class Carte {
 		}
 		if(y!=HAUTEUR)
 			throw new TailleMapException("Problème de Hauteur : "+y+" a la place des "+HAUTEUR+" demandés.");
-//		System.out.println(blockMap.size());
 	}
+	
+	
+	public void detruireBlock(Ressource ressource, int indice) throws ErreurInventairePlein, ErreurObjetIntrouvable {
+		this.blockMap.remove(indice);
+		this.poubelle.ajouter(ressource);
+	}
+	
+	public boolean enDehorsMap(Ressource ressource) {
+		return ressource.getX() < 0 || ressource.getY() > 0;
+	}
+	
+	public void ressourceEnDehorsMap() throws ErreurInventairePlein, ErreurObjetIntrouvable {
+		for(int i = 0 ; i < this.getInventaire().size(); i++) {
+			if(this.enDehorsMap(this.getInventaire().get(i))){
+				this.detruireBlock(this.getInventaire().get(i), i);
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
-
