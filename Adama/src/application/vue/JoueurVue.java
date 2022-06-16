cca3b69cb1464069f370784b852cc8a56c19a7f2
llichten29 <llichten29@gmail.com>
2022-06-16 09:23:53 +0200

    Suppresion de PNJvue
    J'ai supprimer pnjvue pour le remplacer par Personnagevue et j'ai fait que joueurvue extends personnageVue pour éviter la duplication de code

--- Adama/src/application/vue/JoueurVue.java
+++ Adama/src/application/vue/JoueurVue.java
@@ -1,38 +1,20 @@
 package application.vue;
 
-import javafx.geometry.NodeOrientation;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
-public class JoueurVue {
-
-	private ImageView sprite;
+public class JoueurVue extends PersonnageVue {
 
 
 	public JoueurVue() {
-		this.sprite = new ImageView("ressource/perso.png");
+		super("perso");
 	}
 
-	public ImageView getSprite() {
-		return sprite;
-	}
 	
 	public void sautVue(int temps) {
 		if (temps<1) 
-			this.sprite.setImage(new Image("ressource/persoAccroupi.jpg"));
+			this.getsprite.setImage(new Image("ressource/persoAccroupi.jpg"));
 		else if (temps==10) 
 			this.sprite.setImage(new Image("ressource/perso.png"));
 	}
 	
-	public void orrientationSpriteGauche() {
-		if (sprite.effectiveNodeOrientationProperty().get()==NodeOrientation.RIGHT_TO_LEFT)
-			sprite.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
-	}
-
-	public void orrientationSpriteDroite() {
-		if (sprite.effectiveNodeOrientationProperty().get()==NodeOrientation.LEFT_TO_RIGHT)
-			sprite.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
-	}
 
 	
 }
---
+++ Adama/src/application/vue/PersonnageVue.java
@@ -0,0 +1,31 @@
+package application.vue;
+
+import javafx.geometry.NodeOrientation;
+import javafx.scene.image.ImageView;
+
+public class PersonnageVue {
+
+	private ImageView sprite;
+
+
+	public PersonnageVue(String nom) {
+		this.sprite = new ImageView("ressource/"+nom+".png");
+	}
+	
+	public ImageView getSprite() {
+		return sprite;
+	}
+	
+	
+	
+	public void orrientationSpriteGauche() {
+		if (sprite.effectiveNodeOrientationProperty().get()==NodeOrientation.RIGHT_TO_LEFT)
+			sprite.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+	}
+
+	public void orrientationSpriteDroite() {
+		if (sprite.effectiveNodeOrientationProperty().get()==NodeOrientation.LEFT_TO_RIGHT)
+			sprite.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+	}
+
+}
